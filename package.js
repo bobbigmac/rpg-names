b5de9ff4e942f817f27d2088b374fc8d35c9f6eb
bobbigmac <admin@bobbigmac.com>
2015-09-23 23:19:28 +0100

    Update meteor version requirement for 1.2.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'bobbigmac:rpg-names',
-  version: '0.1.1',
+  version: '0.1.2',
   // Brief, one-line summary of the package.
   summary: 'Generates random names for RPG characters. Starting with wizards.',
   // URL to the Git repository containing the source code for this package.
@@ -15,7 +15,7 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.1');
+  api.versionsFrom('1.2.0.1');
 
   api.addAssets('lists/wizard-names.json', ['server']);
   api.addAssets('lists/dragon-names.json', ['server']);
